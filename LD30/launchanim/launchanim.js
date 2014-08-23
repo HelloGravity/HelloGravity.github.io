3fdcd486b6e479f081c12ad4eefe3c11dda203bd
Yaron <yaron.mic@gmail.com>
2014-08-23 16:32:57 +0300

    some more changes

---
+++ LD30/launchanim/launchanim.js
@@ -0,0 +1,16 @@
+function launchanim(game) {
+	this.init = function() {
+		console.log('aha')
+		//var anim = game.add.sprite(800, 600, 'launchanim');
+		//anim.animations.add('run');
+		//anim.animations.play('run', 24, false);
+	}
+	this.cleanup = function() {
+	}
+	this.update = function() {
+	}
+
+	this.nextScreen = function() {
+		return null;
+	}
+}
