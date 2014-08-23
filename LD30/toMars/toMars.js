f82695bae59c774a215301d6d59e22831ce11106
Yaron <yaron.mic@gmail.com>
2014-08-23 16:06:32 +0300

    Some more changes, everything more realistic

--- LD30/toMars/toMars.js
+++ LD30/toMars/toMars.js
@@ -11,13 +11,12 @@ function toMars(game) {
 		player.anchor.setTo(0.5, 0.5);
 		player.rotation = Math.PI / 2;
 		player.scale.set(-0.4,0.4);
-		function spawner() {
+		(function spawner() {
 			if(isActive) {
 				that.spawnPotato();
 				setTimeout(spawner, 300);
 			}
-		}
-		spawner();
+		})();
 	}
 	this.cleanup = function() {
 		game.load.removeAll();
@@ -40,18 +39,41 @@ function toMars(game) {
 			player.y += 5*Math.cos(player.rotation);
 			player.x -= 5*Math.sin(player.rotation);
 		}
+		
+		var index = potatos.length;
+		while(index--) {
+			var potato = potatos[index];
+			if(Phaser.Rectangle.intersects(potato.getBounds(),game.camera.bounds))
+				potato.alreadySeen = true;
+			else if(potato.alreadySeen == true) {
+				potato.destroy();
+				potatos.splice(index, 1);
+			}
+		}
+		/*
+		var seenPotatos = _.where(potatos,"alreadySeen");
+		_.forEach(seenPotatos, function(potato) {
+			game.physics.arcade.collide(potato, seenPotatos);
+		});
+*/
+		_.forEach(potatos, function(potato) {
+			game.physics.arcade.collide(potato, potatos);
+		});
 	}
 
 	this.spawnPotato = function() {
+		if(potatos.length > 10)
+			return;
 		var inner = game.rnd.angle()/180*Math.PI, outer = game.rnd.angle()/180*Math.PI;
 		var beginningX = game.world.centerX + 1000*Math.cos(outer), beginningY = game.world.centerY + 1000*Math.sin(outer);
 		var directionX = game.world.centerX + 300*Math.cos(inner), directionY = game.world.centerY + 300*Math.sin(inner);
 		var potato = game.add.sprite(beginningX, beginningY, 'tomars-potato'+game.rnd.between(1,4));
 		game.physics.arcade.enable(potato);
 		potato.anchor.setTo(0.5, 0.5);
-		potato.scale.set(0.4,0.4);
-		potato.body.velocity.x = (directionX - beginningX)/10;
-		potato.body.velocity.y = (directionY - beginningY)/10;
+		potato.scale.x = potato.scale.y = (game.rnd.frac()*0.7+0.3) * 0.4;
+		var velocityFactor = 0.07*(game.rnd.frac()*0.4+0.6);
+		var speed = Math.sqrt((directionX - beginningX)*velocityFactor * (directionX - beginningX)*velocityFactor + (directionY - beginningY)*velocityFactor*(directionY - beginningY)*velocityFactor);
+		game.physics.arcade.accelerateToXY(potato, directionX, directionY, speed, 3*speed, 3*speed); 
 		potatos.push(potato);
 	}
 
