4c07c2e61fba0f7feb47f915ef3fbd69f186e651
Yaron <yaron.mic@gmail.com>
2014-08-23 13:10:09 +0300

    player is moving

--- LD30/toMars/toMars.js
+++ LD30/toMars/toMars.js
@@ -1,18 +1,12 @@
 function toMars(game) {
-	var player;
+	this.player;
 	this.init = function() {
-		//game.load.image('background', 'toMars/background.png');
-		game.load.image('spaceship', 'toMars/spaceship.png');
-		//game.load.image('potato1', 'toMars/potato1.png');
-		//game.load.image('potato2', 'toMars/potato2.png');
-		//game.load.image('potato3', 'toMars/potato3.png');
-		//game.load.image('potato4', 'toMars/potato4.png');
-
-		player = game.add.sprite(200, 400, 'spaceship');
+		player = game.add.sprite(200, 400, 'tomars-spaceship');
 		game.physics.arcade.enable([player]);
 		player.body.collideWorldBounds = true;
 		player.anchor.setTo(0.5, 0.5);
 		player.rotation = Math.PI / 2;
+		player.scale.set(0.4,0.4);
 	}
 	this.cleanup = function() {
 		game.load.removeAll();
@@ -20,14 +14,17 @@ function toMars(game) {
 	}
 	this.update = function() {
 		if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-			player.rotation += Math.PI / 180 / 3;
+			player.rotation -= Math.PI / 180;
 		if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-			player.rotation -= Math.PI / 180 / 3;
+			player.rotation += Math.PI / 180;
 		if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-			player.x += 10*Math.cos(player.rotation);
-			player.y += 10*Math.sin(player.rotation);
+			player.y -= 5*Math.cos(player.rotation);
+			player.x += 5*Math.sin(player.rotation);
 		}
-
+		if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+			player.y += 5*Math.cos(player.rotation);
+			player.x -= 5*Math.sin(player.rotation);
+		}
 	}
 
 	this.nextScreen = function() {
