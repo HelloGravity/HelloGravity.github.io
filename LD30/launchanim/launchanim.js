e8acc7a89cff0f6bed51409124f860d4692a51d4
Yaron <yaron.mic@gmail.com>
2014-08-23 16:48:08 +0300

    test

--- LD30/launchanim/launchanim.js
+++ LD30/launchanim/launchanim.js
@@ -1,13 +1,20 @@
 function launchanim(game) {
 	this.init = function() {
 		console.log('aha')
-		//var anim = game.add.sprite(800, 600, 'launchanim');
-		//anim.animations.add('run');
-		//anim.animations.play('run', 24, false);
+		var anim0 = game.add.sprite(800, 600, 'launchanim0');
+		anim0.animations.add('run0');
+		anim0.animations.play('run0', 24, false);
 	}
 	this.cleanup = function() {
 	}
 	this.update = function() {
+		if(anim0.isFinished()) {
+			anim0.destroy();
+			var anim1 = game.add.sprite(800, 600, 'launchanim1');
+			anim1.animations.add('run1');
+			anim1.animations.play('run1', 24, false);
+
+		}
 	}
 
 	this.nextScreen = function() {
