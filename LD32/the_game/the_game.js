501a600bd856b46693647a417ee7581c96e813ef
Yaron <yaron.mic@gmail.com>
2015-04-18 20:05:05 +0300

    Added some really ugly code

--- LD32/the_game/the_game.js
+++ LD32/the_game/the_game.js
@@ -1,12 +1,16 @@
 function the_game(game) {
+	var BOOMERANG_RADIUS = 100;
+
 	var player;
 	var ground;
-	var is_going_left = false;
-	var last_is_going_left = false;
+	var boomerang;
+	var boomerang_coordinate;
+	var last_velocity = 0;
+	var current_velocity = 0;
 	var rainbows = [];
 
-	this.init = function() {
-		/* create ground */
+	function create_ground()
+	{
 		ground = game.add.sprite(0,game.height, 'ground');
 		ground.anchor.set(.5,.5)
 		ground.y -= 0.5*ground.height
@@ -15,9 +19,10 @@ function the_game(game) {
    	  	ground.body.immovable = true;
    	  	ground.body.moves = false;
    	  	ground.body.height *= 0.2;
-		
+	}
 
-		/* craete player */
+	function create_player()
+	{
 		player = game.add.sprite(72,72, 'meow-the-cat');
 		player.animations.add('fly');
 		player.x = 200	
@@ -31,31 +36,19 @@ function the_game(game) {
 		this.player.body.collideWorldBounds = true;
 
 		/* handle spacebar */
-		game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(function() {
+		game.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(function() {
 			player.body.velocity.y = -300;
 		}, this);
-
-		this.player = player
-		this.rainbows = rainbows
-		this.ground = ground	
 	}
-	this.cleanup = function() {
-		game.input.keyboard.removeKey(Phaser.Keyboard.SPACEBAR);
-		clear_all_rainbows();
-		if(player) player.destroy();
-	}
-	this.update = function() {
-		game.physics.arcade.collide(player, ground);
+	
+	function create_boomerang_coordinate()
+	{
 
-		do_rainbow();
-		do_meow();
 	}
 
-	this.nextScreen = function() {
-		if(false) {
-			return new endScreen(game);
-		}
-		return null;
+	function create_boomerang()
+	{
+		boomerang = game.add.sprite(72,72, 'meow-the-cat');
 	}
 
 	function clear_all_rainbows()
@@ -66,17 +59,20 @@ function the_game(game) {
 		rainbows = [];
 	}
 
-	function do_rainbow()
+	function add_rainbow(x,y)
 	{
 		/* add rainbows */
 		var new_rainbow = game.add.sprite(1,7, 'rainbow');
 		game.physics.enable(new_rainbow, Phaser.Physics.ARCADE);
-		new_rainbow.x = player.x + player.width * (is_going_left ? 0.2 : 0.27);
-		new_rainbow.y = player.body.center.y;
-		new_rainbow.body.velocity.x = is_going_left ? 60: -60;
-		new_rainbow.scale.set(4,4);
+		new_rainbow.x = x;
+		new_rainbow.y = y;
+		new_rainbow.body.velocity.x = player.scale.x * 60;
 		rainbows.push(new_rainbow);
-		
+	}
+	
+	function do_rainbow()
+	{
+		add_rainbow(player.x + player.width * ((player.scale.x > 0) ? 0.2 : 0.27),player.body.center.y);
 		_(rainbows).forEach(function(rainbow) {
 		 	rainbow.alpha *= 0.95;
 		}).remove(function(rainbow) {
@@ -88,10 +84,10 @@ function the_game(game) {
 
 	function do_meow()
 	{	
-		var left_pressed = game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
-		var right_pressed = game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+		var left_pressed = game.input.keyboard.isDown(Phaser.Keyboard.A);
+		var right_pressed = game.input.keyboard.isDown(Phaser.Keyboard.D);
 
-		
+		last_velocity = player.body.velocity.x;
 		if(left_pressed)
 		{
 			player.body.velocity.x = -100;
@@ -104,13 +100,11 @@ function the_game(game) {
 		{
 			player.body.velocity.x = 0;	
 		}
+		current_velocity = player.body.velocity.x;
 
-		is_going_left = player.body.velocity.x < 0;
-		if (is_going_left ^ last_is_going_left)
-		{
+		if (current_velocity * last_velocity < 0) {
 			clear_all_rainbows();
 		}
-		last_is_going_left = is_going_left;
 
 		/* change meow's animation according to velocity */
 		if (player.body.velocity.y < 0) {
@@ -130,4 +124,57 @@ function the_game(game) {
 		}
 		player.bringToTop();
 	}
+
+	function do_boomerang()
+	{
+		boomerang.bringToTop();
+	}
+
+	function do_boomerang_coordinate()
+	{
+		var difX = game.input.x - player.x;
+		var difY = game.input.y - player.y;
+		var difAbs = Math.sqrt(difX*difX+difY*difY);
+		boomerang_coordinate.x = player.x + BOOMERANG_RADIUS * difX/difAbs;
+		boomerang_coordinate.y = player.y + BOOMERANG_RADIUS * difY/difAbs;
+	}
+
+	this.init = function() {
+		game.stage.backgroundColor = 0x8888FF;
+
+		create_ground();
+		create_player();
+		create_boomerang();
+		create_boomerang_coordinate();
+
+		/* this is here only for debugging */
+		this.player = player
+		this.rainbows = rainbows
+		this.ground = ground	
+	}
+	
+	this.cleanup = function() {
+		game.input.keyboard.removeKey(Phaser.Keyboard.W);
+		clear_all_rainbows();
+		if(player) player.destroy();
+		game.stage.backgroundColor = 0;
+	}
+	
+	this.update = function() {
+		game.physics.arcade.collide(player, ground);
+
+		do_rainbow();
+		do_meow();
+		do_boomerang();
+		do_boomerang_coordinate();
+	}
+
+	this.nextScreen = function() {
+		if(false) {
+			return new endScreen(game);
+		}
+		return null;
+	}
+
+	
 }
