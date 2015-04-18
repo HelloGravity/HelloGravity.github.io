e024fde38bdec2c3cf4c6645cca9ffd109652465
Yaron <yaron.mic@gmail.com>
2015-04-18 15:10:48 +0300

    Got flappy working

--- LD32/the_game/the_game.js
+++ LD32/the_game/the_game.js
@@ -4,21 +4,32 @@ function the_game(game) {
 	this.init = function() {
 		player = game.add.sprite(36,36, 'meow-the-cat');
 		player.animations.add('fly');
-		player.animations.play('fly', 17, true);
 		game.physics.enable(player, Phaser.Physics.ARCADE);
-		player.body.gravity.y = 80;
+		player.body.gravity.y = 120;
 		this.player = player;
 	}
 	this.cleanup = function() {
 		if(player) player.destroy();
 	}
 	this.update = function() {
+		/* handle spacebar presses */
 		var is_space_pressed = game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR);
 		if ((!last_is_space_pressed) && is_space_pressed)
 		{
 			player.body.velocity.y = -150
 		}
 		last_is_space_pressed = is_space_pressed;
+
+		/* change animation according to velocity */
+		if (player.body.velocity.y < 0)
+		{
+			player.animations.play('fly', 17, true);
+		}
+		else
+		{
+			player.animations.stop('fly');
+			player.frame = 0;
+		}
 	}
 
 	this.nextScreen = function() {
