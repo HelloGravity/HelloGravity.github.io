da882f685b1e87905906e159a9e127f33db0ade3
Yaron <yaron.mic@gmail.com>
2015-04-18 18:37:40 +0300

    Added functionallity

--- LD32/the_game/the_game.js
+++ LD32/the_game/the_game.js
@@ -1,12 +1,29 @@
 function the_game(game) {
 	var player;
+	var ground;
+	var is_going_left = false;
+	var last_is_going_left = false;
 	var rainbows = [];
 
 	this.init = function() {
+		/* create ground */
+		ground = game.add.sprite(0,game.height, 'ground');
+		ground.anchor.set(.5,.5)
+		ground.y -= 0.5*ground.height
+		ground.x += 0.5*ground.width
+   	  	game.physics.enable(ground, Phaser.Physics.ARCADE);
+   	  	ground.body.immovable = true;
+   	  	ground.body.moves = false;
+   	  	ground.body.height *= 0.2;
+		
+
+		/* craete player */
 		player = game.add.sprite(72,72, 'meow-the-cat');
 		player.animations.add('fly');
-		player.x = 30
-		player.y = 500
+		player.x = 200	
+		player.y = 200
+		player.anchor.setTo(.5,.5);
+		player.scale.x = -1;
 		game.physics.enable(player, Phaser.Physics.ARCADE);
 		player.body.gravity.y = 320;
 		this.player = player;
@@ -18,30 +35,20 @@ function the_game(game) {
 			player.body.velocity.y = -300;
 		}, this);
 
-		/* handle rainbow presses */
-		game.input.keyboard.addKey(Phaser.Keyboard.C).onDown.add(function() {
-			rainbows.push
-		}, this);
+		this.player = player
+		this.rainbows = rainbows
+		this.ground = ground	
 	}
 	this.cleanup = function() {
 		game.input.keyboard.removeKey(Phaser.Keyboard.SPACEBAR);
-		game.input.keyboard.removeKey(Phaser.Keyboard.C);
+		clear_all_rainbows();
 		if(player) player.destroy();
 	}
 	this.update = function() {
-		/* handle rainbow key presses */
-		 game.input.keyboard.isDown(Phaser.Keyboard.C)
-	
-		/* change meow's animation according to velocity */
-		if (player.body.velocity.y < 0)
-		{
-			player.animations.play('fly', 17, true);
-		}
-		else
-		{
-			player.animations.stop('fly');
-			player.frame = 0;
-		}
+		game.physics.arcade.collide(player, ground);
+
+		do_rainbow();
+		do_meow();
 	}
 
 	this.nextScreen = function() {
@@ -49,5 +56,78 @@ function the_game(game) {
 			return new endScreen(game);
 		}
 		return null;
+	}
+
+	function clear_all_rainbows()
+	{
+		_.forEach(rainbows, function(rainbow) {
+		 	rainbow.destroy();
+		});
+		rainbows = [];
+	}
+
+	function do_rainbow()
+	{
+		/* add rainbows */
+		var new_rainbow = game.add.sprite(1,7, 'rainbow');
+		game.physics.enable(new_rainbow, Phaser.Physics.ARCADE);
+		new_rainbow.x = player.x + player.width * (is_going_left ? 0.2 : 0.27);
+		new_rainbow.y = player.body.center.y;
+		new_rainbow.body.velocity.x = is_going_left ? 60: -60;
+		new_rainbow.scale.set(4,4);
+		rainbows.push(new_rainbow);
+		
+		_(rainbows).forEach(function(rainbow) {
+		 	rainbow.alpha *= 0.95;
+		}).remove(function(rainbow) {
+		  return !rainbow.inCamera || rainbow.alpha < 0.01;
+		}).forEach(function(rainbow) {
+		 	rainbow.destroy();
+		}).value();
+	}
+
+	function do_meow()
+	{	
+		var left_pressed = game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+		var right_pressed = game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+
+		
+		if(left_pressed)
+		{
+			player.body.velocity.x = -100;
+		}
+		if(right_pressed)
+		{
+			player.body.velocity.x = 100;
+		}
+		if (!right_pressed && !left_pressed)
+		{
+			player.body.velocity.x = 0;	
+		}
+
+		is_going_left = player.body.velocity.x < 0;
+		if (is_going_left ^ last_is_going_left)
+		{
+			clear_all_rainbows();
+		}
+		last_is_going_left = is_going_left;
+
+		/* change meow's animation according to velocity */
+		if (player.body.velocity.y < 0) {
+			player.animations.play('fly', 17, true);
+		}
+		else {
+			player.animations.stop('fly');
+			player.frame = 0;
+		}
+		if (player.body.velocity.x > 0)
+		{
+			player.scale.x = -1;
+		}
+		if (player.body.velocity.x < 0)
+		{
+			player.scale.x = 1;
+		}
+		player.bringToTop();
 	}
 }
