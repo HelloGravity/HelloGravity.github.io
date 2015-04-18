c05ba24f1bb74e8ee721b329bd9796ed8df1590d
Yaron <yaron.mic@gmail.com>
2015-04-18 14:38:16 +0300

    Added some stuff

--- LD32/the_game/the_game.js
+++ LD32/the_game/the_game.js
@@ -1,15 +1,26 @@
 function the_game(game) {
 	var player;
-
+	var last_is_space_pressed = false;
 	this.init = function() {
 		player = game.add.sprite(36,36, 'meow-the-cat');
 		player.animations.add('fly');
-		player.animations.play('fly', 17, true);
+		player.animations.play('fly');
+		game.physics.enable(player, Phaser.Physics.ARCADE);
+		//player.body.gravity.y = 40;
+		this.player = player;
 	}
 	this.cleanup = function() {
 		if(player) player.destroy();
 	}
 	this.update = function() {
+		var is_space_pressed = game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR);
+		if (last_is_space_pressed == is_space_pressed)
+		{
+
+		}
+
+
+		last_is_space_pressed = is_space_pressed;
 	}
 
 	this.nextScreen = function() {
