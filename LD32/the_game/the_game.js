28694d616aea9d59b9181332c88a9aac7b0c5bd6
Yaron <yaron.mic@gmail.com>
2015-04-18 13:48:54 +0300

    changed stuff

---
+++ LD32/the_game/the_game.js
@@ -0,0 +1,23 @@
+function the_game(game) {
+	var player;
+
+	this.init = function() {
+		player = game.add.sprite(36,36, 'meow-the-cat');
+		player.animations.add('fly');
+		player.animations.play('fly');
+		game.physics.enable(ground, Phaser.Physics.ARCADE);
+	}
+	this.cleanup = function() {
+		if(player) player.destroy();
+	}
+	this.update = function() {
+	}
+
+	this.nextScreen = function() {
+		if(false) {
+			return new endScreen(game);
+		}
+		return null;
+	}
+	
+}
